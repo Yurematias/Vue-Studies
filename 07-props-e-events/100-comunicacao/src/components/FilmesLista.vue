c44b0e29049f501fc7f009a66c07a414ef36e76a
yurematias <yurematias@ufrn.edu.br>
2019-12-23 17:43:59 -0300

    update

---
+++ 07-props-e-events/100-comunicacao/src/components/FilmesLista.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="row">
+    <!-- coluna 1 -->
+    <div class="col-8">
+      <h2>Filmes</h2>
+      <div class="list-group list-group-flush">
+        <!-- é possível usar o v-bind no props se quisermos referenciar um atributo data deste componente -->
+        <FilmesListaIten v-for="movie of moviesList" :key="movie.id" :movieTitle="movie.title" />
+      </div>
+    </div>
+    <!-- coluna 2 -->
+    <div class="col-4">
+      <FilmesListaItenInfo/>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import FilmesListaIten from './FilmesListaIten.vue'
+import FilmesListaItenInfo from './FilmesListaItenInfo.vue'
+
+export default {
+  components: {
+    FilmesListaIten,
+    FilmesListaItenInfo
+  }, 
+  data() {
+    return {
+      moviesList: [
+        {title: 'El Camino', id: 0},
+        {title: 'Coringa', id: 1},
+        {title: 'O Senhor dos Aneis', id: 2},
+        {title: 'Django Livre', id: 3},
+        {title: 'Avatar', id: 4},
+        {title: 'Planeta dos Macacos', id: 5},
+        {title: 'Tropa de Elite', id: 6},
+        {title: 'Star Wars', id: 7}
+      ]
+    }
+  }
+}
+</script>
---
+++ 07-props-e-events/100-comunicacao/src/components/FilmesListaIten.vue
@@ -0,0 +1,14 @@
+<template>
+    <li class="list-group-item">
+        <span>{{movieTitle}}</span>
+        <button class="btn btn-success float-right">Editar</button>
+    </li>
+</template>
+
+<script>
+export default {
+    props : [
+        'movieTitle'
+    ]
+}
+</script>
