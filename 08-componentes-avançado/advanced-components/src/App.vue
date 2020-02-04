0919c78410872f28fe5c0694e74e9a6a574ee1d0
yurematias <yurematias@ufrn.edu.br>
2020-02-04 16:37:29 -0300

    aulas de slot

--- 08-componentes-avançado/advanced-components/src/App.vue
+++ 08-componentes-avançado/advanced-components/src/App.vue
@@ -1,4 +1,22 @@
 <template>
-  <p>Hello World</p>
+  <div>
+    <!-- todo conteúdo colocado dentro de um componente desta forma -->
+    <!-- irá para a tag slot do componente -->
+    <!-- assim no componente que neste caso é o Post onde quer que coloquemos a tag <slot />  -->
+    <!-- este conteúdo aparecerá -->
+    <Post> 
+      <p>este é o conteúdo passado para dentro do slot no componente Post</p>
+    </Post>  
+  </div>
 </template>
 
+<script>
+
+import Post from './components/Post.vue'
+
+export default {
+  components: {
+    Post
+  }
+}
+</script>
---
+++ 08-componentes-avançado/advanced-components/src/components/Post.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <p>ESTE É UM EXEMPLO USANDO SLOTS</p>
+        <slot />
+    </div>
+</template>
+
+<style scoped>
+    .slot{
+        background-color: red;
+        color: white;
+    }
+</style>>
+    
+
+
